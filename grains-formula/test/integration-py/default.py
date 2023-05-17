b16e27282efa9acd12afbf727047f17b4cbbabfb
Georg Pfuetzenreuter <georg.pfuetzenreuter@suse.com>
2023-05-17 02:14:56 +0200

    grains: add testinfra prototype
    This will probably be dropped again, but as an alternative to InSpec,
    the Python based testinfra could be used.
    
    Signed-off-by: Georg Pfuetzenreuter <georg.pfuetzenreuter@suse.com>

---
+++ grains-formula/test/integration-py/default.py
@@ -0,0 +1,34 @@
+import ast
+import os
+import pytest
+import testinfra
+import yaml
+
+@pytest.fixture
+def host():
+    return testinfra.get_host('docker://{KITCHEN_USERNAME}@{KITCHEN_CONTAINER_ID}'.format(**os.environ))
+
+@pytest.fixture
+def file():
+    return '/etc/salt/grains'
+
+def test_grains_file_exists(host, file):
+    with host.sudo():
+        exists = host.file(file).exists
+    assert exists is True
+
+def test_grains_file_contents(host, file):
+    with host.sudo():
+        struct = host.file(file).content.decode('UTF-8')
+    data = yaml.safe_load(struct)
+    assert data['snack'] == 'peanuts' and data['treats'][0] == 'chocolate'
+
+def test_grains_file_ownership(host, file):
+    with host.sudo():
+        user = host.file(file).user
+    assert user == 'root'
+
+def test_grains_salt(host, file):
+    with host.sudo():
+        snack = host.salt('grains.get', 'snack', local=True)
+    assert snack == 'peanuts'
