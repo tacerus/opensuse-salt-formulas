openSUSE/salt-formulas

commit 97b0fc248572f3ab67fd465b66f6d4a44a6a05a9

infrastructure.salt: move GPG handling to master
This is applicable to all Salt Masters, not only Syndics.

Signed-off-by: Georg Pfuetzenreuter <georg.pfuetzenreuter@suse.com>

infrastructure-formula/infrastructure/salt/master.sls
@@ -54,3 +54,21 @@ salt_git_gc:
     - name: salt-git-gc.timer
     - enable: true
 {%- endif %}
+
+{%- set id = grains['id'] %}
+{%- set gpg_script = '/usr/local/sbin/create_salt_master_gpg_key.sh' %}
+
+install_gpg_bootstrap_script:
+  file.managed:
+    - name: {{ gpg_script }}
+    - source: salt://{{ slspath }}/files{{ gpg_script }}
+    - mode: '0750'
+
+run_gpg_bootstrap_script:
+  cmd.run:
+    - name: {{ gpg_script }} -m salt@{{ id }} -n '{{ id }} (Salt Master Key)'
+    - unless: gpg2 --homedir /etc/salt/gpgkeys -k salt@{{ id }} 1>/dev/null 2>/dev/null
+    - require:
+        - file: install_gpg_bootstrap_script
+    - watch_in:
+        - file: /etc/salt/gpgkeys

infrastructure-formula/infrastructure/salt/syndic.sls
@@ -2,21 +2,3 @@ include:
   - salt.gitfs.pygit2
   - salt.syndic
   - .master
-
-{%- set id = grains['id'] %}
-{%- set gpg_script = '/usr/local/sbin/create_salt_master_gpg_key.sh' %}
-
-install_gpg_bootstrap_script:
-  file.managed:
-    - name: {{ gpg_script }}
-    - source: salt://{{ slspath }}/files{{ gpg_script }}
-    - mode: '0750'
-
-run_gpg_bootstrap_script:
-  cmd.run:
-    - name: {{ gpg_script }} -m salt@{{ id }} -n '{{ id }} (Salt Master Key)'
-    - unless: gpg2 --homedir /etc/salt/gpgkeys -k salt@{{ id }} 1>/dev/null 2>/dev/null
-    - require:
-      - file: install_gpg_bootstrap_script
-    - watch_in:
-      - file: /etc/salt/gpgkeys

tacerus/opensuse-salt-formulas

commit 282067ec9bd94f584b975a9f1d02c66f413955e4

infrastructure.salt: guard GPG handling
The GPG handling applies to all Salt masters only in setups without a
Master of Masters. As we do not have states to differentiate between the
two in the formula, introduce a pillar toggle to allow for deactivation
where GPG is not needed.

Fixes: 97b0fc248572 ("infrastructure.salt: move GPG handling to master")
Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

infrastructure-formula/infrastructure/salt/master.sls
@@ -55,8 +55,9 @@ salt_git_gc:
     - enable: true
 {%- endif %}
 
-{%- set id = grains['id'] %}
 {%- set gpg_script = '/usr/local/sbin/create_salt_master_gpg_key.sh' %}
+{%- if salt['pillar.get']('infrastructure:salt:master:gpg', True) %}
+  {%- set id = grains['id'] %}
 
 install_gpg_bootstrap_script:
   file.managed:
@@ -72,3 +73,20 @@ run_gpg_bootstrap_script:
         - file: install_gpg_bootstrap_script
     - watch_in:
         - file: /etc/salt/gpgkeys
+
+{%- else %}
+
+  {%- if salt['file.file_exists']('/etc/salt/gpgkeys') %}
+salt_gpg_backup:
+  file.copy:
+    - name: /etc/salt/gpgkeys.bak
+    - source: /etc/salt/gpgkeys
+
+salt_gpg_purge:
+  file.absent:
+    - names:
+        - {{ gpg_script }}
+        - /etc/salt/gpgkeys
+  {%- endif %}
+
+{%- endif %}